83a479f9e72552162f2556ccea0f17e0f8725609
VSzabo82 <40633060+VSzabo82@users.noreply.github.com>
2021-08-31 21:51:53 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 23 19:01:00 2021
-
-@author: amadeu
+@author: @author: Scheppach Amadeu, Szabo Viktoria, To Xiao-Yin
 """
 
 import os
@@ -33,43 +31,41 @@ import torch
 import torch.nn as nn
 import numpy as np 
 
+# import own functions
 import data_preprocessing as dp
-
-
 import utilss
 from utilss import create_exp_dir
 
-# import models.SCINet as model
-# --h=2 --K=1 --L=3 --padding=4 --seed=4321 --data_directory=data\traffic.txt
 
 parser = argparse.ArgumentParser("train SCINet")
 # Data and save information
 parser.add_argument('--data_directory', type=str, default='data/traffic.txt', help='location of the data corpus')
 parser.add_argument('--model', type=str, default='SCINet', help='path to save the model')
+parser.add_argument('--data_name', type=str, default='traffic', help='name of data used')
 parser.add_argument('--save', type=str,  default='EXP',
                     help='path to save the final model')
 # Specification of Training 
-parser.add_argument('--horizon', type=int, default=3, help='prediction horizon') # 3, 6, 12, 24 in paper
-parser.add_argument('--epochs', type=int, default=10, help='num of training epochs') # 100
+parser.add_argument('--horizon', type=int, default=3, help='prediction horizon') 
+parser.add_argument('--epochs', type=int, default=10, help='num of training epochs') 
 parser.add_argument('--val_epochs', type=int, default=10, help='num of validation epochs')
 parser.add_argument('--num_steps', type=int, default=2, help='number of iterations per epoch')
 parser.add_argument('--val_num_steps', type=int, default=2, help='number of iterations per valing epoch')
 parser.add_argument('--report_freq', type=int, default=5, help='validation report frequency') # 5 in paper
 # General hyperparameters
-parser.add_argument('--batch_size', type=int, default=2, help='batch size') # 16 in paper
-parser.add_argument('--seq_size', type=int, default=168, help='sequence size') # 200 oder 1000 -> paper:168 as look-back window? And should we call it T to resemble the paper?
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate') # 0.0005 in paper
-parser.add_argument('--k', type=int, default=5, help='kernel size') # 5 in paper
-parser.add_argument('--stride', type=int, default=1, help='stride') # 1 in paper
+parser.add_argument('--batch_size', type=int, default=2, help='batch size') 
+parser.add_argument('--seq_size', type=int, default=168, help='sequence size')
+parser.add_argument('--learning_rate', type=float, default=0.0005, help='init learning rate') 
+parser.add_argument('--k', type=int, default=5, help='kernel size')
+parser.add_argument('--stride', type=int, default=1, help='stride')
 parser.add_argument('--padding', type=int, default=4, help='padding')
-parser.add_argument('--num_motifs', type=int, default=100, help='number of channels') # 320
+parser.add_argument('--num_motifs', type=int, default=100, help='number of channels')
 # Paper specific hyperparameters
-parser.add_argument('--h', type=int, default=2, help='extention of input channel') # 2 in paper
-parser.add_argument('--K', type=int, default=1, help='number of stacks') # 1 in paper
-parser.add_argument('--L', type=int, default=3, help='Number of SCI-Block levels') # 3 in paper
+parser.add_argument('--h', type=int, default=2, help='extention of input channel')
+parser.add_argument('--K', type=int, default=1, help='number of stacks')
+parser.add_argument('--L', type=int, default=3, help='Number of SCI-Block levels')
 # Other
 parser.add_argument('--note', type=str, default='try', help='note for this run')
-parser.add_argument('--seed', type=int, default=4321, help='random seed') # 4321 in paper
+parser.add_argument('--seed', type=int, default=4321, help='random seed')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
 
 parser.add_argument('--model_path', type=str,  default='./run1.pth',
@@ -88,7 +84,6 @@ def Train(model, train_loader, optimizer, criterion, device, num_steps, report_f
     
     # run *num_steps* training steps 
     # train_loader = train_queue
-    # num_steps=3
     for idx, (inputs, targets) in enumerate(train_loader):
         if idx > num_steps:
             break
@@ -97,11 +92,11 @@ def Train(model, train_loader, optimizer, criterion, device, num_steps, report_f
         # reshape for consistent size in calculation
         input = input.reshape(args.batch_size, 1, args.seq_size)
         #y_true = y_true.reshape(args.batch_size, horizon, 1)
-        #y_true = y_true.reshape(args.batch_size, 1, args.horizon)
+        y_true = y_true.reshape(args.batch_size, 1, args.horizon)
         
         # train the model
         model.train()
-        input = input.to(device)#.cuda()
+        input = input.to(device)
         y_true = y_true.to(device)
         
         # predict, calculate loss, update weights
@@ -130,11 +125,10 @@ def Valid(model, valid_loader, optimizer, criterion, device, num_steps, report_f
             input, y_true = inputs, labels
             # reshape for consistent size in calculation
             input = input.reshape(args.batch_size, 1, args.seq_size)
-            #y_true = y_true.reshape(args.batch_size, horizon, 1)
             #y_true = y_true.reshape(args.batch_size, 1, args.horizon)
 
             # predict, calculate loss, save value
-            input = input.to(device)#.cuda()  
+            input = input.to(device)
             y_true = y_true.to(device)
 
             y_pred = model(input.float())
@@ -144,22 +138,15 @@ def Valid(model, valid_loader, optimizer, criterion, device, num_steps, report_f
 
 
 
-
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    # preprocess data
-    train_queue, val_queue, test_queue = dp.data_preprocessing(args.data_directory, args.seq_size, args.batch_size, args.horizon)
+    global model
+    train_queue, val_queue, test_queue = dp.data_preprocessing(args.data_directory, args.seq_size, args.batch_size, args.horizon, args.data_name)
         
-    # define hyperparameters
-    
     if args.model=='SCINet':
+        import models.SCINet as model
         
-        global model
-        
-        #import models.SCINet as model
-        import models.SCINet2 as model
-        
+        # define hyperparameters
         net_args = {
             "in_channels" : 1, # because traffic data one dimensional 
             "expand" : args.h, 
@@ -170,20 +157,17 @@ def main():
             "seq_size" : args.seq_size, 
             "batch_size" : args.batch_size,
             "SCI_Block" : model.SCI_Block, 
-            #"K" : args.K, 
+            "K" : args.K, 
             "L" : args.L,
             "horizon" : args.horizon
             }
         # define model, loss, and optimizer
-        # model = model.stackedSCI(**net_args).float().to(device)
-        model = model.SCI_Net(**net_args).float().to(device)
+        model = model.stackedSCI(**net_args).float().to(device)
         
     if args.model=='baseline_CNN':
-         global model
          import models.baseline_model as model
          model = model.baseline_CNN(args.batch_size, args.horizon, args.seq_size).to(device)
                  
-    #criterion = nn.MSELoss().to(device)
     criterion = nn.L1Loss().to(device) # L1loss is used in paper
     optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
     # initialize loss 
@@ -210,26 +194,8 @@ def main():
     trainloss_file = '{}-train_loss-{}'.format(args.save, train_start)
     np.save(trainloss_file, train_losses)  
     
-    
       
-    
     
 if __name__ == '__main__':
   main() 
   
-    
-class RSELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-    def forward(self, predict, target):
-        return torch.sqrt((predict-target).pow(2).sum())/torch.sqrt((target-target.mean()).pow(2).sum())
-    
-class Correlation(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-    def forward(self, predict, target):
-        target_dev = target-target.mean()
-        predict_dev = predict-predict.mean()
-        return (target_dev*predict_dev).sum()/torch.sqrt((target_dev.pow(2)*predict_dev.pow(2)).sum())
